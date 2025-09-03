9122c27e9124da7feae50a5fe3e7a769224582dc
Nikoloz Gagua <ngagua@gmail.com>
2025-09-03 10:32:06 +0400

    chore: add 20 and 26

---
+++ solutions/20-valid-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  let stack = [];
+  const map = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  };
+  for (let char of s) {
+    if (map[char]) {
+      stack.push(map[char]);
+    } else {
+      if (stack.pop() !== char) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
